79e9d14577b5fea4bf4476a3e1ed491cf22719cd
ShurieJr <shuuriyejr@gmail.com>
2026-09-19 14:37:05 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
+
     static void main(String[] args) {
-        //create an object
+//        //create an object
         Student student1 = new Student();
-        Student student2 = new Student("Hawa mohamed" ,
-                "C11213" , "+278687" , 3.8 , true);
-        Student student3 = new Student();
+        Student student2 = new Student("Hawa mohamed",
+                "C11213", "+278687", 3.8, true);
 
-        //student1.display();
+        student1 = student2;
 
-        System.out.println(student1.toString());
-        System.out.println(student2.toString());
-        System.out.println(student3.toString());
+        System.out.println(student1);
+        System.out.println(student2);
+
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,29 +1,77 @@
 public class Student {
     //data fields - variables
-    String name;
-    String ID;
-    String tel;
-    double gpa;
-    boolean status;
+    private String name;
+    private String ID;
+    private String tel;
+    private double gpa;
+    private boolean status;
+
     //constructors
-    Student(){
+    Student() {
         name = "Mohamed abdullahi";
         ID = "C112160";
         tel = "+2526152948";
         gpa = 3.9;
         status = true;
     }
-    Student(String newName , String newID ,
-            String newTel , double newGpa , boolean newStatus){
-       name = newName;
-       ID = newID;
-       tel = newTel;
-       gpa = newGpa;
-       status = newStatus;
+
+    Student(String newName, String newID,
+            String newTel, double newGpa, boolean newStatus) {
+        name = newName;
+        ID = newID;
+        tel = newTel;
+        gpa = newGpa;
+        status = newStatus;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    //Setters
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public void setGpa(double gpa) {
+        if(gpa >= 0 && gpa <= 4)
+            this.gpa = gpa;
+        else
+            System.out.println("gpa must between 0-4");
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     //methods
-    void display(){
+    void display() {
         System.out.println("ID: " + ID);
         System.out.println("name: " + name);
         System.out.println("tel: " + tel);
